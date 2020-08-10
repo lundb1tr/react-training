0ede857e52eee3eb96d87abcea71fa99ab560c6a
Tyler Lundberg <tylerl@fireflyint.com>
2020-08-10 09:06:08 -0400

    removed error generation from the person component

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,10 +2,6 @@ import React from 'react';
 import classes from './Person.css';
 
 const Person = ({ name, age, children, click, changed }) => {
-  const rnd = Math.random();
-  if (rnd > 0.7) {
-    throw new Error( 'Something went wrong' );
-  }
   return (
     <div className={classes.Person}>
       <p onClick={click}>
