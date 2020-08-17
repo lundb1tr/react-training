01d184d4e63625f2e128b5a9c572b63997b335c7
Tyler Lundberg <tylerl@fireflyint.com>
2020-08-17 08:38:46 -0400

    changeCounter is the wrong way to set state

--- src/containers/App.js
+++ src/containers/App.js
@@ -20,6 +20,7 @@ class App extends Component {
     otherState: 'some other value',
     showPersons: false,
     showCockpit: true,
+    changeCounter: 0,
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -55,7 +56,10 @@ class App extends Component {
 
     persons[personIndex] = person;
 
-    this.setState({ persons: persons });
+    this.setState({
+      persons: persons,
+      changeCounter: this.state.changeCounter + 1,
+    });
   };
 
   deletePersonHandler = personIndex => {
